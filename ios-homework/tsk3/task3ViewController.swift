48b54d04313090efbaecaf741ef4d9e2d42ce0d9
Ruslan <ruslan99992222@gmail.com>
2023-11-14 17:54:10 +0300

    gotovo

--- ios-homework/tsk3/task3ViewController.swift
+++ ios-homework/tsk3/task3ViewController.swift
@@ -12,22 +12,9 @@ class task3ViewController: UIViewController {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBOutlet weak var contentView: UIView!
-//    var contentView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        contentView = UIView()
-//        contentView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(contentView)
-//        
-//        NSLayoutConstraint.activate([
-//                    
-//                    contentView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
-//                    contentView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-//                    contentView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-//                    contentView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-//                ])
         
         showContentView(forSegment: segmentedControl.selectedSegmentIndex)
     }
@@ -46,7 +33,7 @@ class task3ViewController: UIViewController {
         switch index {
         case 0:
             let greenView = UIView()
-            greenView.backgroundColor = .green
+            contentView.backgroundColor = .green
             
             let textField1 = UITextField()
             textField1.placeholder = "Текстовое поле 1"
@@ -56,74 +43,91 @@ class task3ViewController: UIViewController {
             textField2.placeholder = "Текстовое поле 2"
             textField2.borderStyle = .roundedRect
             
-            greenView.addSubview(textField1)
-            greenView.addSubview(textField2)
+            contentView.addSubview(textField1)
+            contentView.addSubview(textField2)
             
-            // Настройка constraints для зелёной view
             textField1.translatesAutoresizingMaskIntoConstraints = false
             textField2.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview(greenView)
             NSLayoutConstraint.activate([
                 
-                textField1.topAnchor.constraint(equalTo: greenView.topAnchor, constant: 20),
-                textField1.centerXAnchor.constraint(equalTo: greenView.centerXAnchor),
+                textField1.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+                textField1.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
                 
                 textField2.topAnchor.constraint(equalTo: textField1.bottomAnchor, constant: 20),
-               textField2.centerXAnchor.constraint(equalTo: greenView.centerXAnchor),
+               textField2.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
 
             ])
             
         case 1:
             let blueView = UIView()
-            blueView.backgroundColor = .blue
+            contentView.backgroundColor = .blue
             
             let button1 = UIButton(type: .system)
             button1.setTitle("Кнопка 1", for: .normal)
-            
+            button1.backgroundColor = .purple
             let button2 = UIButton(type: .system)
             button2.setTitle("Кнопка 2", for: .normal)
+            button2.backgroundColor = .purple
             
-            blueView.addSubview(button1)
-            blueView.addSubview(button2)
+            contentView.addSubview(button1)
+            contentView.addSubview(button2)
             
-            // Настройка constraints для синей view
             button1.translatesAutoresizingMaskIntoConstraints = false
             button2.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview(blueView)
 
             NSLayoutConstraint.activate([
-                button1.topAnchor.constraint(equalTo: blueView.topAnchor, constant: 20),
-                button1.centerXAnchor.constraint(equalTo: blueView.centerXAnchor),
+                button1.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+                button1.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
                 
                 button2.topAnchor.constraint(equalTo: button1.bottomAnchor, constant: 20),
-                button2.centerXAnchor.constraint(equalTo: blueView.centerXAnchor),
+                button2.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             ])
             
         case 2:
             let purpleView = UIView()
-            purpleView.backgroundColor = .purple
+            contentView.backgroundColor = .purple
             
             let imageView1 = UIImageView(image: UIImage(named: "Image1"))
             let imageView2 = UIImageView(image: UIImage(named: "Image2"))
             
-            purpleView.addSubview(imageView1)
-            purpleView.addSubview(imageView2)
+            contentView.addSubview(imageView1)
+            contentView.addSubview(imageView2)
             
-            // Настройка constraints для фиолетовой view
+        
             imageView1.translatesAutoresizingMaskIntoConstraints = false
             imageView2.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview(purpleView)
+            
+               let newImageSize = CGSize(width: 180, height: 180)
+              
+               if let resizedImage1 = imageView1.image?.scaled(toSize: newImageSize),
+                  let resizedImage2 = imageView2.image?.scaled(toSize: newImageSize) {
+                   imageView1.image = resizedImage1
+                   imageView2.image = resizedImage2
+               }
+            
             NSLayoutConstraint.activate([
-                imageView1.topAnchor.constraint(equalTo: purpleView.topAnchor, constant: 20),
-                imageView1.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
+                imageView1.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+                imageView1.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
                 
                 imageView2.topAnchor.constraint(equalTo: imageView1.bottomAnchor, constant: 20),
-                imageView2.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
+                imageView2.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
   
             ])
             
         default:
             break
         }
+    }
+}
+
+extension UIImage {
+    func scaled(toSize newSize: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: newSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
