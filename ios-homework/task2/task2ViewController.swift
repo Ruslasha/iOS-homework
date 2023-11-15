610aa4fb55eb740c3075b3ba69f11e4b48216d63
Ruslan <ruslan99992222@gmail.com>
2023-11-15 14:43:05 +0300

    1,2,3

--- ios-homework/task2/task2ViewController.swift
+++ ios-homework/task2/task2ViewController.swift
@@ -12,5 +12,35 @@ class task2ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        let leftLabel = UILabel()
+        leftLabel.text = "тексттексттексттексттексттексттекст"
+        leftLabel.backgroundColor = .green
+        leftLabel.translatesAutoresizingMaskIntoConstraints = false
+        leftLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        stackView.addArrangedSubview(leftLabel)
+        
+        let rightLabel = UILabel()
+        rightLabel.text = "тексттексттексттексттексттекст"
+        rightLabel.backgroundColor = .blue
+        rightLabel.translatesAutoresizingMaskIntoConstraints = false
+        rightLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        stackView.addArrangedSubview(rightLabel)
+        
+        rightLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 40).isActive = true
+        
+        NSLayoutConstraint.activate([
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8)
+        ])
+        
     }
 }
