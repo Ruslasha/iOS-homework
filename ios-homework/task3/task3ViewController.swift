7cad0b299e4510827cbeab6092dff36def4d361c
Ruslan <ruslan99992222@gmail.com>
2023-11-15 11:07:35 +0300

    1 задание

---
+++ ios-homework/task3/task3ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  task3ViewController.swift
+//  ios-homework
+//
+//  Created by Руслан Абрамов on 15.11.2023.
+//
+
+import UIKit
+
+class task3ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
